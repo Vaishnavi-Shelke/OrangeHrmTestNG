6f08c8b37bbd5222b589fdc68bd66feb58b88f33
Vaishnavi Shelke <shelkevaishnavi2320@gmail.com>
2024-02-29 21:31:57 +0530

    first commit

---
+++ src/test/java/Axis/OrangeHRM/ImmiAddAttachment.java
@@ -0,0 +1,40 @@
+package Axis.OrangeHRM;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+
+public class ImmiAddAttachment extends Login{
+
+	@AfterMethod
+	public void AddAttach() {
+		WebDriver driver = Login.driver;
+		
+		WebElement Immi=driver.findElement(By.xpath("(//div[@class=\"orangehrm-tabs-wrapper\"])[5]"));
+		Immi.click();
+		System.out.println("Clicked on Immigration");
+		WebElement Addattach = driver.findElement(By.xpath("(//button[@class=\"oxd-button oxd-button--medium oxd-button--text\"])[2]"));
+		Addattach.click();
+		System.out.println("Clicked on Add Attachemnts");
+		WebElement Browse = driver.findElement(By.xpath("(//input[@type='file'])[1]"));
+		Browse.sendKeys("C:\\Users\\Admin\\Picture\\Image.jpg");
+		
+		WebElement comments = driver.findElement(By.xpath("//textarea[@placeholder='Type comment here']"));
+		comments.click();
+		comments.sendKeys("File Uploaded");
+		System.out.println("Box");
+		WebElement save = driver.findElement(By.xpath("//button[normalize-space()='Save']"));
+		save.click();
+		System.out.println("Attachment is Saved");
+
+		
+		
+}
+
+		
+	
+}
+
+
